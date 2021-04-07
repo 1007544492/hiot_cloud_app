153217ab661365a9a391d7560c3991b7703b32de
ccy <1007544492@qq.com>
2021-04-07 14:40:54 +0800

    修改登录语句

--- app/src/main/java/com/example/myapplication/test/mvptest/TestPresenter.java
+++ app/src/main/java/com/example/myapplication/test/mvptest/TestPresenter.java
@@ -9,9 +9,9 @@ public class TestPresenter {
     }
     public void login(User user){
         if("ccy".equals(user.getUserName()) && "123".equals(user.getPasswd())){
-            view.showMessage("登陆成功");
+            view.showMessage("登陆成功!!!");
         }else{
-            view.showMessage("登陆失败");
+            view.showMessage("登陆失败!!!");
         }
     }
 }
